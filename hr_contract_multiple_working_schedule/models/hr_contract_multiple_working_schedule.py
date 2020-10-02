445cf2637c5d4340bb0ece17f9d4c57f15c3a21c
Andhitia Rama <andhitia.r@gmail.com>
2020-10-02 11:25:34 +0700

    [hr_contract_multiple_working_schedule] 8.0.1.0.0

---
+++ hr_contract_multiple_working_schedule/models/hr_contract_multiple_working_schedule.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class HrContractMultipleWorkingSchedule(models.Model):
+    _name = "hr.contract_multiple_working_schedule"
+    _description = "Contract Multiple Working Schedule"
+
+    contract_id = fields.Many2one(
+        string="# Employee Contract",
+        comodel_name="hr.contract",
+        required=True,
+    )
+    working_schedule_policy_id = fields.Many2one(
+        string="Working Schedule Policy",
+        comodel_name="hr.working_schedule_policy",
+        required=True,
+    )
+    working_schedule_id = fields.Many2one(
+        string="Working Schedule",
+        comodel_name="resource.calendar",
+        required=True,
+    )
