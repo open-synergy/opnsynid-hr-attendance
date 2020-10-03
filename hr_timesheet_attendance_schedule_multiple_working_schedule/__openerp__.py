818d183356b08d72968f0cbb143bd379a71ad2c2
Andhitia Rama <andhitia.r@gmail.com>
2020-10-03 15:57:18 +0700

    [hr_timesheet_attendance_schedule_multiple_working_schedule] 8.0.1.0.0

---
+++ hr_timesheet_attendance_schedule_multiple_working_schedule/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Timesheet Attendance Schedule Integration With "
+            "Contract Multiple Working Schedule",
+    "version": "8.0.1.0.1",
+    "category": "Human Resource",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr_contract_multiple_working_schedule_mass_generate",
+        "hr_timesheet_attendance_schedule",
+    ],
+    "data": [
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
