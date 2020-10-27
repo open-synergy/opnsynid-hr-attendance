0dd2a9b9f64400d1327376cca13336903408c0cf
Andhitia Rama <andhitia.r@gmail.com>
2020-10-28 06:14:54 +0700

    [hr_holiday_multiple_approval] 8.0.1.0.0

---
+++ hr_holiday_multiple_approval/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "HR Holiday Multiple Approval",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "depends": [
+        "hr_holiday_notebook",
+        "hr_holiday_configuration_page",
+        "base_workflow_policy",
+    ],
+    "data": [
+        "data/hr_holidays_workflow.xml",
+        "data/base_workflow_policy_data.xml",
+        "views/hr_holiday_config_setting_views.xml",
+        "views/hr_holidays_view.xml",
+        "views/hr_holidays_status_view.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+    "post_init_hook": "set_double_validation_false",
+}
---
+++ hr_holiday_multiple_approval/models/res_company.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ResCompany(models.Model):
+    _name = "res.company"
+    _inherit = "res.company"
+
+    leave_request_confirm_grp_ids = fields.Many2many(
+        string="Allowed To Confirm Leave Request",
+        comodel_name="res.groups",
+        relation="rel_company_2_leave_request_confirm_group",
+        column1="company_id",
+        column2="group_id",
+    )
+    leave_request_cancel_grp_ids = fields.Many2many(
+        string="Allowed To Cancel Leave Request",
+        comodel_name="res.groups",
+        relation="rel_company_2_leave_request_cancel_group",
+        column1="company_id",
+        column2="group_id",
+    )
+    leave_request_restart_grp_ids = fields.Many2many(
+        string="Allowed To Approve Leave Request",
+        comodel_name="res.groups",
+        relation="rel_company_2_leave_request_restart_group",
+        column1="company_id",
+        column2="group_id",
+    )
+    leave_request_restart_val_grp_ids = fields.Many2many(
+        string="Allow To Restart Leave Request",
+        comodel_name="res.groups",
+        relation="rel_company_2_leave_request_restart_val_group",
+        column1="company_id",
+        column2="group_id",
+    )
+    leave_allocation_confirm_grp_ids = fields.Many2many(
+        string="Allowed To Confirm Leave Allocation",
+        comodel_name="res.groups",
+        relation="rel_company_2_leave_allocation_confirm_group",
+        column1="company_id",
+        column2="group_id",
+    )
+    leave_allocation_cancel_grp_ids = fields.Many2many(
+        string="Allowed To Cancel Leave Allocation",
+        comodel_name="res.groups",
+        relation="rel_company_2_leave_allocation_cancel_group",
+        column1="company_id",
+        column2="group_id",
+    )
+    leave_allocation_restart_grp_ids = fields.Many2many(
+        string="Allowed To Approve Leave Allocation",
+        comodel_name="res.groups",
+        relation="rel_company_2_leave_allocation_restart_group",
+        column1="company_id",
+        column2="group_id",
+    )
+    leave_allocation_restart_val_grp_ids = fields.Many2many(
+        string="Allow To Restart Leave Allocation",
+        comodel_name="res.groups",
+        relation="rel_company_2_leave_allocation_restart_val_group",
+        column1="company_id",
+        column2="group_id",
+    )
