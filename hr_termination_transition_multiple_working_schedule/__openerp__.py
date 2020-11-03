232af41876ea4b407df226edff456d3eddcf258f
mikevhe18 <viriyananda.michael@gmail.com>
2020-11-03 10:31:50 +0700

    [8.0.1.0.0][hr_termination_transition_multiple_working_schedule]

---
+++ hr_termination_transition_multiple_working_schedule/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Termination Transition - Multiple Working Schedule Integration",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "hr_termination_transition",
+        "hr_career_transition_multiple_working_schedule",
+    ],
+    "data": [
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
